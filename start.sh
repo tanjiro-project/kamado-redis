95c176599f5ca82016e3c7d4cffd02ef345c4977
KChi <59391215+KagChi@users.noreply.github.com>
2021-11-23 17:03:09 +0700

    chore: update startup script

--- start.sh
+++ start.sh
@@ -1,7 +1,14 @@
-wget http://download.redis.io/redis-stable.tar.gz
-tar xvzf redis-stable.tar.gz
-rm -rf redis-stable.tar.gz
-cd redis-stable
-make distclean
-make
-src/redis-server
+if [ ! -d "redis-stable" ]; then
+    wget http://download.redis.io/redis-stable.tar.gz
+    tar xvzf redis-stable.tar.gz
+    rm -rf redis-stable.tar.gz
+else 
+    cd redis-stable
+    if [ -e "src/redis-server" ]; then
+         src/redis-server --requirepass "$REDIS_AUTH"
+    else 
+         make distclean
+         make
+         src/redis-server --requirepass "$REDIS_AUTH"
+    fi
+fi
