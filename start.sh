4134eb7c3240d163901e13a5070e186d19d80eca
KChi <59391215+KagChi@users.noreply.github.com>
2021-11-23 17:15:39 +0700

    chore: update startup script

--- start.sh
+++ start.sh
@@ -1,14 +1,25 @@
+if [ ! -n "$PORT" ] 
+   then PORT=3000
+fi;
+
 if [ ! -d "redis-stable" ]; then
     wget http://download.redis.io/redis-stable.tar.gz
     tar xvzf redis-stable.tar.gz
     rm -rf redis-stable.tar.gz
-else 
-    cd redis-stable
     if [ -e "src/redis-server" ]; then
-         src/redis-server --requirepass "$REDIS_AUTH"
+         src/redis-server --port "$PORT" --requirepass "$REDIS_AUTH"
     else 
          make distclean
          make
-         src/redis-server --requirepass "$REDIS_AUTH"
+         src/redis-server --port "$PORT" --requirepass "$REDIS_AUTH"
+    fi
+else 
+    cd redis-stable
+    if [ -e "src/redis-server" ]; then
+         src/redis-server --port "$PORT" --requirepass "$REDIS_AUTH"
+    else 
+         make distclean
+         make
+         src/redis-server --port "$PORT" --requirepass "$REDIS_AUTH"
     fi
 fi
